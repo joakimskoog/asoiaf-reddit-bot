6c15887d37f11b61243fe6e045be569d2161134a
Joakim Skoog <glasvegas@hotmail.se>
2016-03-20 21:16:56 +0100

    Created loop and tick

--- asoiaf/bot.py
+++ asoiaf/bot.py
@@ -1,8 +1,22 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/env
+# -*- coding: utf-8 -*-
 
 import anapioficeandfire
 import praw
+import time
+
+
+def _tick(comment):
+    print('Tick')
+
+
+def _loop(reddit):
+    for comment in praw.helpers.comment_stream(reddit, 'asoiaf'):
+        _tick(comment)
+
 
 if __name__ == '__main__':
     print('Initiating bot...')
+    reddit = praw.Reddit('asoiaf-reddit-bot')
+    _loop(reddit)
+    print('Exiting bot...')
