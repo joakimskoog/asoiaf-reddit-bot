e69889f5865ad27cf0bf05ac1fbd9b1a00e899d1
Joakim Skoog <glasvegas@hotmail.se>
2016-03-21 21:39:47 +0100

    Added parsing of comments

--- asoiaf/bot.py
+++ asoiaf/bot.py
@@ -4,10 +4,44 @@
 import anapioficeandfire
 import praw
 import time
+import re
+
+
+def _parse_book(comment):
+    result = re.search('!Book\((.+?)\)', comment.body)
+    if result is not None:
+        return result.group(1).strip()
+
+    return None
+
+
+def _parse_character(comment):
+    result = re.search('!Character\((.+?)\)', comment.body)
+    if result is not None:
+        return result.group(1).strip()
+
+    return None
+
+
+def _parse_house(comment):
+    result = re.search('!House\((.+?)\)', comment.body)
+    if result is not None:
+        return result.group(1).strip()
+
+    return None
 
 
 def _tick(comment):
-    print('Tick')
+    book = _parse_book(comment)
+    character = _parse_character(comment)
+    house = _parse_house(comment)
+
+    if not book:
+        print(book) #Send request to /api/books here
+    if not character:
+        print(character) #Send request to /api/characters here
+    if not house:
+        print(house) #Send request to /api/houses here
 
 
 def _loop(reddit):
